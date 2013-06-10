c9f84e6801ddc46ba637a20c46f9edbe6d9ad50b
magm3333 <magm3333@gmail.com>
2013-06-10 09:04:25 -0300

    script descarga material

---
+++ meteor/curso/descargarMaterial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir meteor
+cd meteor
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/PdfsMeteor.zip?raw=true -O PdfsMeteor.zip
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/PresentacionesMeteor.zip?raw=true -O PresentacionesMeteor.zip
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/SoloCodigo.odt?raw=true -O SoloCodigo.odt
+unzip PdfsMeteor.zip
+unzip PresentacionesMeteor.zip
+rm PdfsMeteor.zip
+rm PresentacionesMeteor.zip
+mkdir proyectos
+cd proyectos
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/proyectos/encapsula.zip?raw=true -O encapsula.zip
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/proyectos/prestamosFinal.zip?raw=true -O prestamosFinal.zip
+wget https://github.com/magm3333/material-osbi/blob/master/meteor/curso/proyectos/prestamosFinalParte1-UnSoloArchivo.zip?raw=true -O prestamosFinalParte1-UnSoloArchivo.zip
+
+
+
+
+
